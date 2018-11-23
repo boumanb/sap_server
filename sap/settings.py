9f82ec74038b082fd69c3a3ed51e1a15f6a81116
BBouman <Billybouman@ziggo.nl>
2018-11-23 22:54:14 +0100

    Added autofixture and first migration:

--- sap/settings.py
+++ sap/settings.py
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sap',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modernrpc'
+    'modernrpc',
+    'autofixture'
 ]
 
 MIDDLEWARE = [
