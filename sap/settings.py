d18b02c8d7825ae3421f58f875a35ecfc9ed06e8
Jaakko Talvitie <jt@jaska.net>
2018-12-04 12:36:19 +0100

    Replaced the database with postgreSQL.

--- sap/settings.py
+++ sap/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'sap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DBNAME'),
         'USER': config('DBUSER'),
         'PASSWORD': config('DBPASS'),
