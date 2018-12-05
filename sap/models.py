52de859695d6ff591045bd0c232853a6d9dd2494
Jaakko Talvitie <jt@jaska.net>
2018-12-05 14:11:20 +0100

    Added the models, connections not ready yet.

--- sap/models.py
+++ sap/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Device(models.Model):
-    name = models.CharField(max_length=200)
+    installation_UID = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -13,6 +13,7 @@ class Device(models.Model):
 class Student(models.Model):
     name = models.CharField(max_length=200)
     card_UID = models.IntegerField()
+    email = models.CharField(max_length=200)
     device = models.OneToOneField(
         Device,
         on_delete=models.CASCADE
@@ -22,3 +23,40 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Class(models.Model):
+    begin_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    room = models.ForeignKey(Room)
+    course = models.ForeignKey(Course)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    reader_UID = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name, reader_UID
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    teacher = models.ForeignKey(Teacher)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher (models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name, self.email
