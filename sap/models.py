663a0f1cfbf83cce7577f0e44c77ee0a84e18bea
Jaakko Talvitie <jt@jaska.net>
2018-12-05 16:09:11 +0100

    Default boolean to False

--- sap/models.py
+++ sap/models.py
@@ -69,5 +69,5 @@ class Collage(models.Model):
 class Attendance(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    phone = models.BooleanField(default=True)
-    card = models.BooleanField(default=True)
+    phone = models.BooleanField(default=False)
+    card = models.BooleanField(default=False)
