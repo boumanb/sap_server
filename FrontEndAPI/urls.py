a4cb4b90357c67a2552d50713f6c2292e0930e22
Leroy numeijer <lnumeije@avans.nl>
2018-12-11 10:20:46 +0100

    initial push

---
+++ FrontEndAPI/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from modernrpc.views import RPCEntryPoint
+from . import views
+
+urlpatterns = [
+
+]
