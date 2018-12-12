df9c3f7b71c05f7ec4ab2ef15e480a570b655156
Jaakko Talvitie <jt@jaska.net>
2018-12-12 14:57:39 +0100

    Make collages function

--- sap/models.py
+++ sap/models.py
@@ -28,7 +28,7 @@ class Student(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def attend(self):
-        # Marks attendace for the student. The booleans default to False.
+        # Marks attendance for the student. The booleans default to False.
         att = Attendance(student=self)
         att.save()
 
@@ -90,6 +90,7 @@ class Teacher(models.Model):
         att = Attendance(student=student, phone_check=True, card_check=True)
 
 
+
 class Course(models.Model):
     name = models.CharField(max_length=200)
     teacher = models.ManyToManyField(Teacher)
@@ -97,9 +98,25 @@ class Course(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     attendees = models.ManyToManyField(Student)
 
-    def make_colleges(self, room, time, dates):
-        return 0
 
+    def make_colleges(self, room, times, dates):
+        # Times is a list of tuples(weekday, starttime, endtime)
+        # Dates is start and end date
+        for e in times:
+            weekday = eval("rrule." + e[0])
+            days = rrule.rrule(rrule.DAILY,
+                               byweekday=weekday,
+                               dtstart=d1,
+                               until=d2)
+            for x in days:
+                coll = Collage(
+                    day=x,
+                    begin_time=e[1],
+                    end_time=e[2],
+                    room=room,
+                    course=self
+                )
+                coll.save()
 
 class Room(models.Model):
     name = models.CharField(max_length=200)
