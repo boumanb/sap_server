a680be126f2536dec096fa8246bb8c5d7efc5961
Billy <billybouman@ziggo.nl>
2019-01-16 15:54:58 +0100

    env base url without trail slash

--- sap/context_processors.py
+++ sap/context_processors.py
@@ -3,5 +3,6 @@ from django.conf import settings
 
 def global_settings(request):
     return {
-        'BASE_URL_W_TRAILING_SLASH': settings.BASE_URL_W_TRAILING_SLASH
+        'BASE_URL_W_TRAILING_SLASH': settings.BASE_URL_W_TRAILING_SLASH,
+        'BASE_URL': settings.BASE_URL_W_TRAILING_SLASH[:-1]
     }
