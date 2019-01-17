f0cf6dcdfbe208d0cc7ac831244fdd23d0d3e4f6
Billy <billybouman@ziggo.nl>
2019-01-17 09:44:40 +0100

    data loading

--- sap/context_processors.py
+++ sap/context_processors.py
@@ -1,8 +1,23 @@
 from django.conf import settings
+from rest_framework.authtoken.models import Token
 
 
 def global_settings(request):
     return {
         'BASE_URL_W_TRAILING_SLASH': settings.BASE_URL_W_TRAILING_SLASH,
-        'BASE_URL': settings.BASE_URL_W_TRAILING_SLASH[:-1]
+        'BASE_URL': settings.BASE_URL_W_TRAILING_SLASH[:-1],
+        'API_URL': settings.BASE_URL_W_TRAILING_SLASH + 'api/'
     }
+
+
+def api_token(request):
+    if request.user.is_authenticated:
+        token = Token.objects.get(user=request.user)
+        return {
+            'API_TOKEN': token
+        }
+    else:
+        return {
+            'API_TOKEN': ''
+        }
+
